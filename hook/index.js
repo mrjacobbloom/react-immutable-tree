05d5e327548140c904a6825f320a9d93dc3e6635
Jacob Bloom <mr.jacob.bloom@gmail.com>
2020-11-17 00:57:49 -0700

    Try to make /hook work a lil better in more places

---
+++ hook/index.js
@@ -0,0 +1,8 @@
+/*
+ * This is an attempt to simulate the /hook export for environments without a
+ * concept of exports
+ */
+
+import { useTree } from '../dist/react-immutable-tree-hook.js';
+
+export { useTree };
