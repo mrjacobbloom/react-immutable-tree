2a76621f6a8b1e2e5c3cda99138766a07ae526da
Jacob Bloom <mr.jacob.bloom@gmail.com>
2020-11-17 17:01:29 -0700

    Hm let's try this

---
+++ hook/index.d.ts
@@ -0,0 +1,6 @@
+/*
+ * This is an attempt to simulate the /hook export for environments without a
+ * concept of exports
+ */
+
+export declare const useTree: typeof import('../src/react-immutable-tree-hook').useTree;
